49cb9bd01db8699302ff6cb14b9ffad64cd2cc34
SeongyeonOh <songyon95@gmail.com>
2019-11-29 21:48:09 +0900

    taxi commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={'myStyle'}>같이 택시 탈사람?</h1>
+        <div className={'post'}>
+          When?  
+          <textarea></textarea>
+          <button className={'submit'} >select</button>
+        </div>
+        <div className={'post'}>
+          Where? 
+          <textarea></textarea>
+          <button className={'submit'}>select</button>
+        </div>
+      
     </div>
   );
 }
